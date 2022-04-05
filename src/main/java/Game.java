656ffc2c3009c1989eea307900788ca83bbdce67
hassyboy2021 <s1136140@student.hsleiden.nl>
2022-04-05 15:15:32 +0200

    wall collision

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,52 +1,91 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class Game extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(600);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+        settings.setVersion("0.1");
+    }
+
+    @Override
+    protected void initInput() {
+        onKey(KeyCode.D, () -> {
+            player.translateX(5); // move right 5 pixels
+            inc("pixelsMoved", +5);
+        });
+
+        onKey(KeyCode.A, () -> {
+            player.translateX(-5); // move left 5 pixels
+            inc("pixelsMoved", -5);
+        });
+
+        onKey(KeyCode.W, () -> {
+            player.translateY(-5); // move up 5 pixels
+            inc("pixelsMoved", +5);
+        });
+
+        onKey(KeyCode.S, () -> {
+            player.translateY(5); // move down 5 pixels
+            inc("pixelsMoved", +5);
+        });
+
+        onKeyDown(KeyCode.F, () -> {
+            play("drop.wav");
+        });
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
 
     private Entity player;
 
     @Override
-    protected void initSettings(GameSettings settings) {
-        settings.setWidth(800);
-        settings.setHeight(800);
-        settings.setTitle("GameSEDungeon");
-        settings.setVersion("1.0");
+    protected void initGame() {
+        player = entityBuilder()
+                .type(EntityType.PLAYER)
+                .at(300, 300)
+                .viewWithBBox(new Rectangle(30, 30, Color.RED))
+                .with(new CollidableComponent(true))
+                .buildAndAttach();
 
-    }
-
-    @Override
-    protected void initGame(){
-        player = FXGL.entityBuilder()
-                .at(400, 400)
-                .view(new Rectangle(30, 30, Color.RED))
+        entityBuilder()
+                .type(EntityType.WALL)
+                .at(560, 0)
+                .viewWithBBox(new Rectangle(40, 600, Color.BROWN))
+                .with(new CollidableComponent(true))
                 .buildAndAttach();
     }
 
     @Override
-    protected void initInput(){
-        FXGL.onKey(KeyCode.D, () -> {
-            player.translateX(5);
-        });
+    protected void initPhysics() {
+        getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityType.PLAYER, EntityType.WALL) {
 
-        FXGL.onKey(KeyCode.A, () -> {
-            player.translateX(-5);
-        });
-
-        FXGL.onKey(KeyCode.W, () -> {
-            player.translateY(-5);
-        });
-
-        FXGL.onKey(KeyCode.S, () -> {
-            player.translateY(5);
+            // order of types is the same as passed into the constructor
+            @Override
+            protected void onCollisionBegin(Entity player, Entity wall) {
+                player.translate(-10,0);
+            }
         });
     }
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
---
+++ src/main/java/GameFactory.java
@@ -0,0 +1,8 @@
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
