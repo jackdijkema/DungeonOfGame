0088b2a8c74ee9f5813c802576fe1bd721cb0766
ricklue <s1135986@student.hsleiden.nl>
2022-04-05 10:53:46 +0200

    Game class opgezet

---
+++ src/main/java/Game.java
@@ -0,0 +1,14 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Game extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
