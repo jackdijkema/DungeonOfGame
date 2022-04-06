2344e5257b20626a8447254bdb446f1bb59ca94a
Jack <jackdijkema@hotmail.com>
2022-04-06 13:31:52 +0200

    made custom menu, title, background, menu buttons

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,13 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import org.jetbrains.annotations.NotNull;
+
 public class Game extends GameApplication {
 
     private Entity player;
@@ -21,14 +22,24 @@ public class Game extends GameApplication {
 
         settings.setTitle("Game Of Dungeon");
         settings.setMainMenuEnabled(true);
+
+        settings.setSceneFactory(new SceneFactory() {
+            @Override
+            public FXGLMenu newMainMenu() {
+                return new DungeonOfGameMainMenu();
+            }
+
+        });
     }
 
-    @Override
-    protected void initGame(){
+    @Override protected void initGame(){
+
         player = FXGL.entityBuilder()
                 .at(400, 400)
                 .view(new Rectangle(30, 30, Color.RED))
                 .buildAndAttach();
+
+
     }
 
     @Override
