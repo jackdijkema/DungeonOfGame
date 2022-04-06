a82e785bb986db7257b4a4482f2967ac12e1d86e
Luuk <Luuk3706@Gmail.com>
2022-04-06 13:03:53 +0200

    Map inladen, Player inladen, player movement added

---
+++ src/main/java/TestFactory.java
@@ -0,0 +1,39 @@
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.*;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
+
+
+public class TestFactory implements EntityFactory{
+
+    @Spawns("wall")
+    public Entity newWall(SpawnData data){
+        return entityBuilder(data)
+                .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+                .with(new PhysicsComponent())
+                .collidable()
+                .build();
+    }
+
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data){
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.DYNAMIC);
+
+        return entityBuilder(data)
+                .type(MapTest.EntityType.PLAYER)
+                .viewWithBBox(new Rectangle(30, 30, Color.BLUE))
+                .collidable()
+                .with(physics)
+                .with(new PlayerComponent())
+                .build();
+    }
+}
